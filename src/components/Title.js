693b7fea506e8a1b9fc18051b6ecdf2396c6b1a9
Miłosz Panas <miloszpanas@gmail.com>
2018-07-13 19:23:42 +0200

    Add all essential files

---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Title = (content, tasks) => (
+    <h2>{content} ({tasks})</h2>
+);
+
+export default Title;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+console.log('aasd');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
